711dd6bc3c129278199fcd20366c50f038bb351f
Yash Parmar <yashparmar15700@gmail.com>
2020-06-27 17:14:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AppBuilder from './containers/AppBuilder/AppBuilder';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+          <AppBuilder />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/containers/AppBuilder/AppBuilder.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Navigation from '../../components/Header/Navigation/Navigation';
+
+const appBuilder = (props) => (
+    <Navigation />
+);
+
+export default appBuilder;
